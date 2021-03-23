aff7b4ea5b99fbb981f7fa8b7f1694c6495852e1
papy21-code <papeibrahimadiop@esp.sn>
2021-03-23 23:47:34 +0000

    PID a modifie

--- Bdd.sql
+++ Bdd.sql
@@ -1,11 +1,24 @@
 CREATE OR REPLACE DATABASE Projet_schema;
 
-CREATE TABLE seance(
-    id_Conference STRING,
-    date_debut DATE,
-    date_fin DATE,
-    partage_ecran INTEGER,
-    note INTEGER,
-    qualite STRING,
-    nb_interaction INTEGER
+CREATE TABLE Seance(
+    Id_Conference STRING primary key,
+    Date_debut DATE,
+    Date_fin DATE,
+    Partage_ecran INTEGER,
+    Note INTEGER,
+    Qualite STRING,
+    Nb_interaction INTEGER
+);
+
+CREATE TABLE Classe(
+    Id_classe STRING primary key,
+    Effectif INTEGER
+);
+
+CREATE TABLE Participant(
+    Nb_Connexion_Deconnexion INTEGER,
+    Duree_Moyenne_Presence INTEGER,
+    Type_terminal STRING,
+    Adresse_mail STRING,
+    Localisation STRING
 );
