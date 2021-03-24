d3da4dc46e05b5246de7296b709c54b3450bc51f
papy21-code <papeibrahimadiop@esp.sn>
2021-03-24 16:49:55 +0000

    PID

--- Bdd.sql
+++ Bdd.sql
@@ -31,9 +31,10 @@ CREATE TABLE Professeur(
     Type_terminal VARCHAR(20),
     Adresse_mail VARCHAR(20),
     Localisation VARCHAR(20)
-    CONSTRAINT fk_Table_Participant FOREIGN KEY (Nb_Connexion_Deconnexion, Duree_Moyenne_Presence,Type_terminal,Adresse_mail,Localisation) REFERENCES Participant(Nb_Connexion_Deconnexion),
+    CONSTRAINT fk_Table_Participant1 FOREIGN KEY (Nb_Connexion_Deconnexion, Duree_Moyenne_Presence,Type_terminal,Adresse_mail,Localisation) REFERENCES Participant(Nb_Connexion_Deconnexion)
 
 );
+
 CREATE TABLE Eleve(
     Nom_utilisateur VARCHAR(20) PRIMARY KEY,
     Nb_Connexion_Deconnexion INTEGER,
@@ -41,18 +42,16 @@ CREATE TABLE Eleve(
     Type_terminal VARCHAR(20),
     Adresse_mail VARCHAR(20),
     Localisation VARCHAR(20),
-    CONSTRAINT fk_Nb_Con_Decon FOREIGN KEY (Nb_Connexion_Deconnexion)  REFERENCES Participant(Nb_Connexion_Deconnexion),
-    
+    CONSTRAINT fk_Table_Participant FOREIGN KEY (Nb_Connexion_Deconnexion, Duree_Moyenne_Presence,Type_terminal,Adresse_mail,Localisation) REFERENCES Participant(Nb_Connexion_Deconnexion)
 );
 
 CREATE TABLE Matiere(
     Id_Matiere VARCHAR(20)
 );
+
 CREATE TABLE Liste_Presence(
     Id_Liste VARCHAR(20) PRIMARY KEY,
     Adresse_Mail VARCHAR(20) ,
     Id_Classe VARCHAR(20),
-    Id_Matiere VARCHAR(20)
-    
-    
+    Id_Matiere VARCHAR(20) 
 );
