75272b945cc9f2dc318d0161b7472574e6a51d2c
boubatra <boubacartraore@esp.sn>
2021-03-25 10:32:33 +0000

    Debut creation du plugin

--- Bdd.sql
+++ Bdd.sql
@@ -1,5 +1,14 @@
 CREATE OR REPLACE DATABASE Projet_schema;
 
-CREATE TABLE seance('
+CREATE TABLE seance(
+    conference_id varchar(6) NOT NULL PRIMARY KEY,
+    --Date_debut
+    --Date_fin
+    screencast_recv_seconds integer(6),
+    note integer(2),
+    qualite varchar(50),
+    
 
-');
+
+
+);     
